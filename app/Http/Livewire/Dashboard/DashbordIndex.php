da20d2fa8ad79b0d3c746ac0e77886f5339a5173
HazemIsmail-Prog <hazem.ismail@hotmail.com>
2023-02-04 02:20:30 +0300

    income chart

--- app/Http/Livewire/Dashboard/DashbordIndex.php
+++ app/Http/Livewire/Dashboard/DashbordIndex.php
@@ -4,17 +4,41 @@ namespace App\Http\Livewire\Dashboard;
 
 use App\Models\Account;
 use App\Models\Payment;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class DashbordIndex extends Component
 {
     public $widgets = [];
+    public $yearList =[];
+    public $year;
+    public $chartData;
 
     public function mount()
     {
-        $this->widgets['Total Income'] = Payment::sum('amount');
+        // $this->yearList = Payment::selectRaw('YEAR(created_at) as year')->distinct()->get();
+        $this->year = now()->format('Y');
+        $this->updatedYear();
         $this->widgets['Bank Balance'] = Account::find(112001)->balance;
         $this->widgets['Cash on Hand'] = Account::find(111027)->balance;
+    }
+
+    public function updatedYear(){
+        $this->yearList = Payment::selectRaw('YEAR(created_at) as year')->distinct()->get();
+        $this->chartData = Payment::query()
+        ->whereYear('created_at', $this->year)
+        ->select(
+            DB::raw("
+            sum(amount) as total, 
+            YEAR(created_at) year, 
+            MONTH(created_at) month
+            ")
+            )
+            ->groupBy('year', 'month')
+            ->get()->toArray();
+            // dd('');
+        $this->dispatchBrowserEvent('loadChart',$this->chartData);
+
     }
     public function render()
     {
--- resources/views/livewire/dashboard/dashbord-index.blade.php
+++ resources/views/livewire/dashboard/dashbord-index.blade.php
@@ -1,4 +1,4 @@
-<div class="container">
+<div class="container-fluid">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -11,14 +11,28 @@
                         </div>
                     @endif
 
+
                     <div class="row">
-                        @foreach ($widgets as $key=>$value)
-                        <div class="col-md-4 mb-2">
+                        <div class="col-md-12 mb-2">
                             <div class="card">
-                                <div class="card-header">{{ $key }}</div>
-                                <div class="card-body">{{ number_format($value,3) }}</div>
+                                <div class="card-header">Income</div>
+                                <div class="card-body">
+                                    <select wire:model="year" class="form-control form-control-sm text-center">
+                                        @foreach ($yearList as $year)
+                                            <option value="{{ $year->year }}">{{ $year->year }}</option>
+                                        @endforeach
+                                    </select>
+                                    <canvas id="barChart"></canvas>
+                                </div>
                             </div>
                         </div>
+                        @foreach ($widgets as $key => $value)
+                            <div class="col-md-6 mb-2">
+                                <div class="card">
+                                    <div class="card-header">{{ $key }}</div>
+                                    <div class="card-body">{{ number_format($value, 3) }}</div>
+                                </div>
+                            </div>
                         @endforeach
                     </div>
                 </div>
@@ -26,3 +40,45 @@
         </div>
     </div>
 </div>
+@push('scripts')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
+    <script>
+        document.addEventListener('livewire:load', function() {
+            loadChart(@js($chartData));
+        });
+
+        document.addEventListener('loadChart', event => {
+            loadChart(event.detail);
+        });
+
+        function loadChart(data) {
+            months = [];
+            totals = [];
+            data.forEach((row) => months.push(row.month));
+            data.forEach((row) => totals.push(row.total));
+            var ctxB = document.getElementById("barChart").getContext('2d');
+            var myBarChart = new Chart(ctxB, {
+                type: 'bar',
+                data: {
+                    labels: months,
+                    datasets: [{
+                        label: 'Income',
+                        data: totals,
+                        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                        borderColor: 'rgba(54, 162, 235, 1)',
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            });
+        }
+    </script>
+@endpush
